150f685aae763127481daf93ff0e8e07fe1ba358
Lajos Uti <lajos.uti@gmail.com>
2010-06-18 15:12:36 +0200

    Fixed output, also did small refactoring

--- dishes/dishes.py
+++ dishes/dishes.py
@@ -4,26 +4,39 @@
 from datetime import date
 from dateutil.rrule import rrule, DAILY
 
+# the people who can be used for some work :)
 NAMES = ["András", "Benno", "Bogdán", "Lajos", "Lóránt", "Krisztián", "Thomas"]
-HOLIDAYS = [date(2010,05,24)]
 
-startDate = date(2010,06,01)
-endDate = date(2010,06,20)
+# the index of the person who should be the first to pick
+# (indexing starts from zero as usual)
+STARTING_INDEX = 1
 
-def isWeekend(date):
-	#if date.
-	pass
+# this is the interval we use to generate the schedule
+START_DATE = date(2010,6,21)
+END_DATE = date(2010,7,10)
+
+# the official public holidays for the year
+HOLIDAYS = [date(2010,8,20), date(2010,10,23), date(2010,11,1), date(2010,12,25), date(2010,12,26)]
+
+def isWeekend(day):
+	if day.weekday() == 5 or day.weekday() == 6:
+		return True
+	return False
+	
+def printThisDayAndName(day, name):
+	print "|-"
+	print "| " + day.strftime("%A %e %B %Y")
+	print "| " + name
 
 if __name__ == "__main__":
-	rr = rrule(DAILY, dtstart=startDate, until=endDate)
-	i = 0
+	rr = rrule(DAILY, dtstart=START_DATE, until=END_DATE)
+	i = STARTING_INDEX
 	for day in list(rr):
-		if day.weekday() == 5 or day.weekday() == 6: continue
-		print "|-"
-		print "| " + str(day)
-		print "| " + NAMES[i]
+		if isWeekend(day) or day in HOLIDAYS: 
+			continue
+		printThisDayAndName(day, NAMES[i])
 		i = i + 1
-		if i == len(NAMES): i = 0
+		if i == len(NAMES): 
+			i = 0
 		
 
-	
