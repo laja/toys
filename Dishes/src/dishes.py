9bdb31957238465f7de9c50646a4785750d7eb55
Lajos Uti <lajos.uti@gmail.com>
2010-09-01 12:49:42 +0200

    fixed the check of holidays

--- Dishes/src/dishes.py
+++ Dishes/src/dishes.py
@@ -5,34 +5,40 @@ from datetime import date
 from dateutil.rrule import rrule, DAILY
 
 # the people who can be used for some work :)
-NAMES = ["András", "Benno", "Bogdán", "Gergely", "Lajos", "Krisztián"]
+NAMES = ["András", "Benno", "Bogdán", "Gábor", "Lajos", "Krisztián"]
 
 # the index of the person who should be the first to pick
 # (indexing starts from zero as usual)
-STARTING_INDEX = 3
+STARTING_INDEX = 5
 
 # this is the interval we use to generate the schedule
-START_DATE = date(2010,8,2)
-END_DATE = date(2010,8,28)
+START_DATE = date(2010,9,6)
+END_DATE = date(2010,10,01)
 
-# the official public holidays for the year
-HOLIDAYS = [date(2010,8,20), date(2010,10,23), date(2010,11,1), date(2010,12,25), date(2010,12,26)]
+# the official public holidays for the year & the days when we are not in
+HOLIDAYS = [date(2010,8,20), date(2010,9,9), date(2010,9,10), date(2010,10,23), date(2010,11,1), date(2010,12,25), date(2010,12,26)]
 
 def isWeekend(day):
 	if day.weekday() == 5 or day.weekday() == 6:
 		return True
 	return False
+
+def isHoliday(day):
+	for holiday in HOLIDAYS:
+		if holiday.isoformat()[0:10] == day.isoformat()[0:10]:
+			return True
+	return False
 	
 def printThisDayAndName(day, name):
 	print "|-"
-	print "| " + day.strftime("%A %e %B %Y")
+	print "| " + day.strftime("%A, %e %B %Y")
 	print "| " + name
 
 if __name__ == "__main__":
 	rr = rrule(DAILY, dtstart=START_DATE, until=END_DATE)
 	i = STARTING_INDEX
 	for day in list(rr):
-		if isWeekend(day) or day in HOLIDAYS: 
+		if isWeekend(day) or isHoliday(day): 
 			continue
 		printThisDayAndName(day, NAMES[i])
 		i = i + 1
